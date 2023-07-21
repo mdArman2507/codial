5cf51ba814d6839fef6fe7a4e8a4f5eae24fbdaa
mdArman2507 <mdarman2507@gmail.com>
2023-07-21 17:00:10 +0530

    add manual-authentication

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,9 +2,29 @@ const User = require('../models/user');
 
 
 module.exports.profile = function (req, res) {
-    return res.render('user_profile', {
-        title: "profile"
-    });
+    // return res.render('user_profile', {
+    //     title: "profile"
+    // });
+    if(req.cookies.user_id)
+    {
+        User.findById(req.cookies.user_id,function(err,user){
+                if(user)
+                {
+                    return res.render('user_profile',{
+                        title:"User profile",
+                        User:user
+                    });
+                }
+                else
+                {
+                    return res.redirect('/users/sign-in');
+                }
+           });
+        }
+        else
+        {
+            return res.redirect('/users/sign-in');
+        }
 }
 
 module.exports.singUp = function (req, res) {
@@ -38,6 +58,30 @@ module.exports.create = function (req, res) {
             });
         }
         else {
+            return res.redirect('back');
+        }
+    });
+}
+
+
+module.exports.createSession=function(req,res){
+    User.findOne({email:req.body.email},function(err,user){
+        if(err)
+        {
+            console.log('error in sign-in');
+            return;
+        }
+        if(user)
+        {
+            if(user.password!=req.body.password)
+            {
+                return res.redirect('back');
+            }
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        }
+        else
+        {
             return res.redirect('back');
         }
     });
--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,6 @@ router.get('/sign-up',userController.singUp);
 router.get('/sign-in',userController.signIn);
 
 router.post('/create',userController.create);
+router.post('/create_session',userController.createSession);
 
 module.exports=router;
