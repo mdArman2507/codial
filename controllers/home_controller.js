3dde2088c52bc8e2eec3105bbba3450c06567717
mdArman2507 <mdarman2507@gmail.com>
2023-07-25 10:19:28 +0530

    modify the code callback to Asyc

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,23 +1,24 @@
-const Post=require('../models/post');
-const User=require('../models/user');
-module.exports.home= async function(req,res)
-{
-    
-    const posts=await Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            res.render('home', {
-                title: 'codiel | Home',
-                posts: posts,
-                all_users:users
-              });
+const Post = require('../models/post');
+const User = require('../models/user');
+module.exports.home = async function (req, res) {
+    try {
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            });
+        let users = await User.find({});
+        res.render('home', {
+            title: 'codiel | Home',
+            posts: posts,
+            all_users: users
         });
-    });
+    }
+    catch (err) {
+        console.log('error', err);;
+        return;
+    }
 }
